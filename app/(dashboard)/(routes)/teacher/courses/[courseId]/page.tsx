545cd4ea7bf34732578ab313a0b867ec9228ee96
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-03-19 19:10:57 +0300

    Create Course Step

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,3 +1,8 @@
+import { Badge } from '@/components/ui/badge'
+import { prismadb } from '@/lib/db'
+import { auth } from '@clerk/nextjs'
+import { LayoutDashboardIcon } from 'lucide-react'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 interface CourseDetailProps{
@@ -6,10 +11,87 @@ interface CourseDetailProps{
     }
 }
 
-const CourseDetail = ({params}:CourseDetailProps) => {
+const CourseDetail = async({params}:CourseDetailProps) => {
+
+  const {userId} = auth();
+
+  if(!userId){
+    return redirect("/");
+  }
+
+  const course = await prismadb.course.findUnique({
+    where:{
+      id:params.courseId
+    }
+  })
+
+  if(!course){
+    return redirect("/");
+  }
+
+  const RequiredFields =[
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId
+
+  ]
+
+  const totalFields = RequiredFields.length;
+
+  const completeFields = RequiredFields.filter(Boolean).length;
+
+  const completeText = `(${completeFields}/${totalFields})`
+
+
+
+
   return (
-    <div>
-        {params.courseId}
+    <div className=''>
+
+      <div className='flex items-center justify-between'>
+        <div className='flex flex-col gap-y-3'>
+          <h1 className='text-3xl md:text-4xl
+           text-purple-600 font-semibold'>Course Setup</h1>
+
+          <span className='text-base'>Complete all fields {completeText}</span>
+
+        </div>
+
+
+
+
+
+      </div>
+
+
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-4 mt-12'>
+
+    {/** Col-span-1 */}
+        <div>
+          <div className='flex items-center gap-2'>
+
+            <Badge variant="mybadge" className='p-4'>
+              <LayoutDashboardIcon className='h-8 w-8 text-purple-700'/>
+            </Badge>
+            <h2 className='text-xl'>Customize your course</h2>
+
+          </div>
+
+
+        </div>
+
+    {/** Col-span-1 */}
+        <div>
+
+
+        </div>
+
+
+
+      </div>
+     
 
     </div>
   )
--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/lib/utils"
 
 const badgeVariants = cva(
-  "inline-flex items-center rounded-full border border-slate-200 px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
+  "inline-flex items-center rounded-full border border-slate-200  text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-slate-950 focus:ring-offset-2 dark:border-slate-800 dark:focus:ring-slate-300",
   {
     variants: {
       variant: {
@@ -15,6 +15,8 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-red-500 text-slate-50 hover:bg-red-500/80 dark:bg-red-900 dark:text-slate-50 dark:hover:bg-red-900/80",
         outline: "text-slate-950 dark:text-slate-50",
+        mybadge: "bg-purple-100",
+
       },
     },
     defaultVariants: {
