4de2dbd05ae2161f824c063d35d7a0905f7fce04
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-04-04 23:20:20 +0300

    Eski hale dönüş

--- lib/iyzico.ts
+++
@@ -1,7 +0,0 @@
-import iyzipay from 'iyzipay';
-
-export const iyziPay = new iyzipay({
-  apiKey: process.env.IYZIPAY_API_KEY!,
-  secretKey: process.env.IYZIPAY_SECRET_KEY!,
-  uri: process.env.IYZIPAY_URI!
-});
